393b9fae4c4ac0eccf98ce39ac5734518b30adb8
ReaGol <reutgoldin@gmail.com>
2024-05-28 20:20:01 +0300

    added fetchExchangeRates

--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,12 @@
 
 
 import axios from "axios";
-
+const EXCHANGERATE_API_KEY = process.env.NEXT_PUBLIC_EXCHANGERATE_API_KEY;
 
 const headers = {
   "Numista-API-Key": process.env.NEXT_PUBLIC_NUMISTA_API_KEY,
 };
+
 export const fetchCoinData = async (query) => {
   try {
     console.log("Fetching coin data for query: ", query);
@@ -22,6 +23,18 @@ export const fetchCoinData = async (query) => {
       "Error fetching coin data:",
       error.response ? error.response.data : error.message
     );
+    return null;
+  }
+};
+
+export const fetchExchangeRates = async (baseCurrency = "USD") => {
+  try {
+    const response = await axios.get(
+      `https://v6.exchangerate-api.com/v6/${EXCHANGERATE_API_KEY}/latest/${baseCurrency}`
+    );
+    return response.data.conversion_rates;
+  } catch (error) {
+    console.error("Error fetching exchange rates:", error);
     return null;
   }
 };
